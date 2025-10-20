fb1432ed7924df0b5538f6b0c1c604091a4ffa9d
EndOfCircuit <nemesisofnocturn@gmail.com>
2025-10-20 22:12:49 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,7 +19,7 @@
  A:::::A                 A:::::Auu::::::::uu:::ur:::::r              ee:::::::::::::e x:::::x    x:::::x i::::::i s:::::::::::ss           UU:::::::::UU    I::::::::I
 AAAAAAA                   AAAAAAA uuuuuuuu  uuuurrrrrrr                eeeeeeeeeeeeeexxxxxxx      xxxxxxxiiiiiiii  sssssssssss               UUUUUUUUU      IIIIIIIIII
 
-___              _                  _   _           ___          _      ___       __ _                         ___              _           
+ ___              _                  _   _           ___          _      ___       __ _                         ___              _           
 |   \ _____ _____| |___ _ __  ___ __| | | |__ _  _  / __| ___ _ _(_)_ _ / __| ___ / _| |___ __ ____ _ _ _ ___  / __| ___ _ ___ _(_)__ ___ ___
 | |) / -_) V / -_) / _ \ '_ \/ -_) _` | | '_ \ || | \__ \/ _ \ '_| | ' \\__ \/ _ \  _|  _\ V  V / _` | '_/ -_) \__ \/ -_) '_\ V / / _/ -_|_-<
 |___/\___|\_/\___|_\___/ .__/\___\__,_| |_.__/\_, | |___/\___/_| |_|_||_|___/\___/_|  \__|\_/\_/\__,_|_| \___| |___/\___|_|  \_/|_\__\___/__/
@@ -32,13 +32,8 @@ by Nebula Softworks
 
 ]]
 
--- Needed function. Delete if everything is up to date with this
-GetIcon = function(...)
-	return Aurexis:GetIcon(...)
-end
 
-
-local BASE_URL = "https://github.com/SorinSoftware-Services/AurexisInterfaceLibrary/edit/Unstable/New/"
+local BASE_URL = "https://github.com/SorinSoftware-Services/AurexisInterfaceLibrary/tree/Unstable/New/"
 
 local Release = "Closed Beta [v 0.1]"
 
